9a4a020f5778e22ebe8de64bb810b0cff2e743ff
Sy Hung NGHIEM <cappuccino2806@gmail.com>
2018-05-21 09:20:57 +0200

    Test build --prod

--- src/app/brand.service.ts
+++ src/app/brand.service.ts
@@ -9,7 +9,9 @@ const SPIDER_NAME = 'topbrand';
 @Injectable()
 export class BrandService {
   private brandsUrl = '';
+
   constructor(private http: HttpClient) { }
+
   runJob(brand: string, country: string) {
     const url = 'https://app.scrapinghub.com/api/run.json?apikey=' + API_KEY;
     const data = 'project=' + PROJECT_ID + '&spider=' + SPIDER_NAME + '&brand=' + brand + '&country=' + country;
@@ -17,16 +19,19 @@ export class BrandService {
       headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
     }).map((res: any) => res);
   }
+
   getJob(job_id?: string): Observable<any> {
     const jobUrl = job_id ? '&job=' + job_id : '';
     const url = 'https://app.scrapinghub.com/api/jobs/list.json?apikey=' + API_KEY + '&project=' + PROJECT_ID +
      '&spider=' + SPIDER_NAME + jobUrl;
     return this.http.get(url).map((res: any) => res);
   }
+
   getBrands(job_id): Observable<any> {
     const url = 'https://storage.scrapinghub.com/items/' + job_id + '?apikey=' + API_KEY + '&format=json';
     return this.http.get(url).map((res: any) => res);
   }
+
   getCountries(): Observable<any> {
     const url = '../assets/countries.json';
     return this.http.get(url).map((res: any) => res);
--- src/app/brands/brands.component.ts
+++ src/app/brands/brands.component.ts
@@ -22,20 +22,21 @@ export class BrandsComponent implements OnInit {
     { name: 'Airlines', val: 'airlines' },
     { name: 'Alcohol', val: 'alcohol' }];
   brandName = this.brandNames[0].val;
+
   constructor(private brandService: BrandService, private router: Router) {
     this.gridOptions = {
       enableSorting: true,
       enableFilter: true,
       rowSelection: 'single',
-      rowHeight: 70,
+      rowHeight: 70
     };
     this.gridOptions.columnDefs = [
       { headerName: '#', field: 'id', cellClass: 'cell-custom', width: 50 },
       { headerName: 'Logo', field: 'logo', cellRenderer: 'logoRenderer', suppressMenu: 'true', suppressSorting: 'true', width: 50 },
-      { headerName: 'Name', field: 'name', cellRenderer: 'brandNameRenderer', cellClass: 'cell-custom' },
-      { headerName: 'Fans', field: 'fans', cellClass: 'cell-custom', width: 100 },
-      { headerName: 'Tags', field: 'tags', cellClass: 'cell-custom' },
-      { headerName: '<i class="fab fa-facebook-square"></i>', field: 'url_page', cellRenderer: 'brandNameRenderer', cellClass: 'cell-custom' }
+      { headerName: 'Name', field: 'name', cellRenderer: 'brandNameRenderer', cellClass: 'cell-custom', width: 150 },
+      { headerName: 'Fans', field: 'fans', cellClass: 'cell-custom', width: 60 },
+      { headerName: 'Tags', field: 'tags', cellClass: 'cell-custom'},
+      { headerName: '<i class="fab fa-facebook-square"></i>', field: 'url_page', cellRenderer: 'brandNameRenderer', cellClass: 'cell-custom'}
     ];
     this.gridOptions.frameworkComponents = {
       logoRenderer: LogoRendererComponent,
@@ -44,6 +45,7 @@ export class BrandsComponent implements OnInit {
     this.gridOptions.overlayLoadingTemplate =
       '<span class="ag-overlay-loading-center">Please wait while your rows are loading</span>';
   }
+
   ngOnInit() {
     this.socialNetworkSite = this.router.url.slice(1);
     this.brandService.getCountries().subscribe(countries => {
@@ -51,24 +53,31 @@ export class BrandsComponent implements OnInit {
       this.country = this.countries[0];
     });
   }
-  runCrawler() {
+
+  changeCriteria() {
     this.gridApi.showLoadingOverlay();
+    // check job with brand and country
+    this.brandService.getJob().subscribe(data => {
+      const existingJob = data.jobs.find(job => this.isExistingJob(job));
+      existingJob ? this.getInfoJob(existingJob.id) : this.runCrawler();
+    });
+  }
+
+  runCrawler() {
     this.brandService.runJob(this.brandName, this.country).subscribe(data => {
       const job_id = data.jobid;
       this.getInfoJob(job_id);
     });
   }
+
+  // check job with job id
   getInfoJob(job_id) {
     this.brandService.getJob(job_id).subscribe(info => {
       const state = info.jobs[0].state;
-      if (state === 'finished') {
-        this.getBrands(job_id);
-        return;
-      } else {
-        setTimeout(() => this.getInfoJob(job_id), 1000);
-      }
+      state === 'finished' ? this.getBrands(job_id) : setTimeout(() => this.getInfoJob(job_id), 1000);
     });
   }
+
   getBrands(job_id) {
     this.brands = [];
     this.brandService.getBrands(job_id).subscribe(
@@ -90,6 +99,7 @@ export class BrandsComponent implements OnInit {
       }
     );
   }
+
   onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
@@ -97,14 +107,15 @@ export class BrandsComponent implements OnInit {
     this.gridApi.showLoadingOverlay();
     this.brandService.getJob().subscribe(data => {
       const jobFrance = data.jobs.find(job => !job.hasOwnProperty('spider_args') ||
-        job.spider_args.country === this.countries[0] && job.spider_args.brand === '');
-      if (!jobFrance) {
-        this.runCrawler();
-        return;
-      }
-      this.getInfoJob(jobFrance.id);
+        this.isExistingJob(job));
+      jobFrance ? this.getInfoJob(jobFrance.id) : this.runCrawler();
     });
   }
+
+  isExistingJob(job) {
+    return job.spider_args.country === this.country && job.spider_args.brand === this.brandName;
+  }
+
   compare(val1, val2) {
     const pos1 = val1.id;
     const pos2 = val2.id;
